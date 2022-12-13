49c1a0c83727e4c59bb41506eeb000bd9cb12d1b
Deniz Orsel <denizonerorsel@gmail.com>
2022-12-13 12:29:17 +0100

    AnimatedCardnNav

---
+++ app.js
@@ -0,0 +1,49 @@
+gsap.registerPlugin(Flip);
+
+const links = document.querySelectorAll(".nav-item a");
+const activeNav = document.querySelector(".active-nav");
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    gsap.to(links, { color: "#252525" });
+    //Turn navs blue
+    if (document.activeElement === link) {
+      gsap.to(link, { color: "#385ae0" });
+    }
+
+    //Flip animation
+    const state = Flip.getState(activeNav);
+    link.appendChild(activeNav);
+    Flip.from(state, {
+      duration: 1.25,
+      absolute: true,
+      ease: 'elastic.out(1, 0.3)',
+    });
+})});
+
+
+// Cards
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach((card, index) => {
+    card.addEventListener("click", () => {
+        const state = Flip.getState(cards);
+        const isCardActive = card.classList.contains("active");
+        cards.forEach((otherCard, otherIndex) => {
+            otherCard.classList.remove("active");
+            otherCard.classList.remove("is-inactive");
+            if(!isCardActive && index !== otherIndex) otherCard.classList.add("is-inactive");
+        });
+        if(!isCardActive) card.classList.add("active");
+
+        Flip.from(state, {
+            duration: 1.25,
+            absolute: true,
+            ease: 'expo.out',
+        });
+
+
+
+    });
+});
